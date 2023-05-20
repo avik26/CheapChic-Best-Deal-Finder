6245f32f4d1bb6dfb1f731ab0ef6c058912a9dab
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-05-20 23:12:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,11 +38,9 @@ def scrape():
     else:
         return jsonify({'error': 'No form found in request'}), 503
 
-
 @app.route('/')
 def index():
     return render_template('index.html')
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=False, threaded=True)
