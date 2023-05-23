c60f35df6b9a39768bd4ab5bc8d009421919123c
Avik Ghosh <130585622+avik26@users.noreply.github.com>
2023-05-23 19:05:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
 
-
 @app.route("/check_connection", methods=['GET', 'POST'])
 def check_connection():
     return jsonify({'status': 'OK'})
